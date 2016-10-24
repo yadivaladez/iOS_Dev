5674d31068ac832f2a049cc245fe9b7101aa8e44
Yadira Valadez <Yadira@rgnt2-98-225-dhcp.int.colorado.edu>
2016-10-24 12:09:39 -0600

    Messing around with small functions

--- exercises/functions.playground/Contents.swift
+++ exercises/functions.playground/Contents.swift
@@ -4,9 +4,40 @@ import UIKit
 
 var str = "Hello, playground"
 
-var num1 = 10
-var num2 = 15
-func addNum(num1, num2){
-    num1 + num2
-    
+
+func addNum(num1, num2, Numsum){
+    let num1 = 5
+    let num2 = 10
+    return num1 + num2
 }
+
+
+var shoppingList = ["catfish", "water", "tulips", "blue paint"]
+shoppingList[1] = "bottle of water"
+
+let individualScores = [75, 43, 103, 87, 12]
+var teamScore = 0
+for score in individualScores {
+    if score > 50 {
+        teamScore += 3
+    } else {
+        teamScore += 1
+    }
+}
+print(teamScore)
+
+let interestingNumbers = [
+    "Prime": [2, 3, 5, 7, 11, 13],
+    "Fibonacci": [1, 1, 2, 3, 5, 8],
+    "Square": [1, 4, 9, 16, 25],
+]
+var largest = 0
+for (kind, numbers) in interestingNumbers {
+    for number in numbers {
+        if number > largest {
+            largest = number
+        }
+    }
+}
+print(largest)
+
