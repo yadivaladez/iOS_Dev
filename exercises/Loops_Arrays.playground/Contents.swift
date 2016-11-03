6a32451aaf4da8ce587b5c2bbe69310de6d47f1b
Yadira Valadez <yadivaladez@gmail.com>
2016-11-03 09:05:18 -0600

    Small changes

--- exercises/Loops_Arrays.playground/Contents.swift
+++ exercises/Loops_Arrays.playground/Contents.swift
@@ -3,7 +3,8 @@
 import UIKit
 
 //Declare an Array of type String
-var firstArray : [String]
+var firstArray: [String]
+firstArray = ["nil"]
 
 // Declare an explicit array of type Double and initialize it with 4 values
 
@@ -14,4 +15,15 @@ secArray = [9.32 ,2,3,6]
 
 var thirdArray = [4.00, 3.95, 4.94, 2.94]
 
-//
+//Use append 3 times in each array
+
+firstArray.append("first")
+secArray.append(7.89)
+thirdArray.append(9.75)
+
+//Use removeAt in each array
+secArray.remove(at: 2)
+
+//Use removeAll on one array
+firstArray.removeAll()
+
